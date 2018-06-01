cf6aafd67bdaf976f2c67d7d69bf11662f6190a8
duongtrungvinh <38530276+duongtrungvinh@users.noreply.github.com>
2018-06-01 21:44:52 +0700

    QLCAOOC-UPLOADCSDL-1.1
    QLCAOOC-UPLOADCSDL-1.1

--- CSDL_QLCAOOC.sql
+++ CSDL_QLCAOOC.sql
@@ -7,6 +7,18 @@ Create database QLCAOOC
 go
 use QLCAOOC
 go
+Create table [DIENTICH] (
+	[MaDienTich] varchar(10) NOT NULL,
+	[TenDienTich] Nvarchar(20) NOT NULL
+Primary Key  ([MaDienTich])
+) 
+go
+Create table [TANG] (
+	[MaTang] varchar(10) NOT NULL,
+	[TenTang] Nvarchar(20) NOT NULL,
+Primary Key  ([MaTang])
+) 
+go
 Create table [GIA] (
 	[MaDienTich] varchar(10) NOT NULL,
 	[MaTang] varchar(10) NOT NULL,
@@ -28,37 +40,42 @@ go
 
 
 Create table [HOPDONG] (
-	[MaHopDong] varchar(10) NOT NULL,
+	[MaHopDong] integer NOT NULL,
 	[NgayBatDauThue] Date NOT NULL,
-	[MaNK] varchar(10) NOT NULL,
+	[NgayHetHan] Date NOT NULL,
+	[MaNK] integer NOT NULL,
 	[MaCanHo] varchar(10) NOT NULL,
 	[GiaPhong] Integer NOT NULL,
 	[ThoiGianThue] Integer NOT NULL,
+	[DaThanhToan] Integer NOT NULL,
 Primary Key  ([MaHopDong])
 ) 
 go
 
 Create table [PHIEUTHUTIEN] (
-	[MaPhieuThu] varchar(10) NOT NULL,
-	[MaHopDong] varchar(10) NOT NULL,
+	[MaPhieuThu] integer NOT NULL,
+	[MaHopDong] integer NOT NULL,
 	[DotThu] Integer NOT NULL,
+	[NgayThu] Date NOT NULL,
+	[SoThangThu] Integer NOT NULL,
 	[SoTien] Integer NOT NULL,
 Primary Key  ([MaPhieuThu])
 ) 
 go
 
 Create table [NHANKHAU] (
-	[MaNK] varchar(10) NOT NULL,
+	[MaNK] integer NOT NULL,
 	[HoTen] Nvarchar(20) NOT NULL,
 	[CMND] Integer NOT NULL,
 	[DienThoai] varchar(20) NOT NULL,
 	[DiaChi] Nvarchar(30) NOT NULL,
 	[HinhAnh] varchar(30) NOT NULL,
-	[MaHopDong] varchar(10) NULL,
+	[MaHopDong] integer NULL,
 	[TenDN] varchar(20) NULL,
 	[MatKhau] varchar(20) NULL,
 Primary Key  ([MaNK])
 ) 
+
 go
 
 Create table [ADMIN] (
@@ -68,15 +85,11 @@ Create table [ADMIN] (
 Primary Key  ([TenDN])
 ) 
 go
-
-Create table [THAMSO] (
-	[MaThamSo] varchar(10) NOT NULL,
-	[GiaTri] varchar(10) NULL,
-	[GhiChu] Nvarchar(40) NULL,
-Primary Key  ([MaThamSo])
-) 
+Alter table [GIA] add  foreign key([MaDienTich]) references [DIENTICH] ([MaDienTich]) 
 go
-Alter table [CANHO] add  foreign key([MaDienTich], [MaTang]) references [GIA] ([MaDienTich], [MaTang]) 
+Alter table [GIA] add  foreign key([MaTang]) references [TANG] ([MaTang]) 
+go
+Alter table [CANHO] add  foreign key([MaDienTich], [MaTang]) references [GIA] ([MaDienTich], [MaTang])
 go
 Alter table [HOPDONG] add  foreign key([MaCanHo]) references [CANHO] ([MaCanHo]) 
 go
@@ -86,6 +99,13 @@ Alter table [PHIEUTHUTIEN] add  foreign key([MaHopDong]) references [HOPDONG] ([
 go
 Alter table [NHANKHAU] add  foreign key([MaHopDong]) references [HOPDONG] ([MaHopDong])
 
+
+INSERT INTO TANG
+VALUES ('1', N'Tầng 1'), ('2', N'Tầng 2'), ('3', N'Tầng 3')
+
+
+INSERT INTO DIENTICH
+VALUES ('A', N'150 mét vuông'), ('B', N'100 mét vuông'), ('C', N'50 mét vuông')
 
 INSERT INTO GIA
 VALUES ('A', '1', 5000000)
@@ -107,20 +127,50 @@ INSERT INTO GIA
 VALUES ('C', '3', 4000000)
 
 INSERT INTO CANHO
-VALUES ('A1', N'Còn trống', 'CHA1.JPG', '', 'A', '1')
+VALUES ('A1', N'Còn trống', 'A1-3.jpg', '', 'A', '1')
 INSERT INTO CANHO
-VALUES ('A2', N'Còn trống', 'CHA2.JPG', '', 'A', '2')
+VALUES ('A2', N'Còn trống', 'A2-3.jpg', '', 'A', '2')
 INSERT INTO CANHO
-VALUES ('A3', N'Còn trống', 'CHA3.JPG', '', 'A', '3')
+VALUES ('A3', N'Còn trống', 'A3-3.jpg', '', 'A', '3')
 INSERT INTO CANHO
-VALUES ('B1', N'Còn trống', 'CHB1.JPG', '', 'B', '1')
+VALUES ('B1', N'Còn trống', 'B1-3.jpg', '', 'B', '1')
 INSERT INTO CANHO
-VALUES ('B2', N'Còn trống', 'CHB2.JPG', '', 'B', '2')
+VALUES ('B2', N'Còn trống', 'B2-3.jpg', '', 'B', '2')
 INSERT INTO CANHO
-VALUES ('B3', N'Còn trống', 'CHB3.JPG', '', 'B', '3')
+VALUES ('B3', N'Còn trống', 'B3-3.jpg', '', 'B', '3')
 INSERT INTO CANHO
-VALUES ('C1', N'Còn trống', 'CHC1.JPG', '', 'C', '1')
+VALUES ('C1', N'Còn trống', 'C1-3.jpg', '', 'C', '1')
 INSERT INTO CANHO
-VALUES ('C2', N'Còn trống', 'CHC2.JPG', '', 'C', '2')
+VALUES ('C2', N'Còn trống', 'C2-3.jpg', '', 'C', '2')
 INSERT INTO CANHO
-VALUES ('C3', N'Còn trống', 'CHC3.JPG', '', 'C', '3')
+VALUES ('C3', N'Còn trống', 'C3-3.jpg', '', 'C', '3')
+
+
+INSERT INTO ADMIN
+VALUES (N'ADMIN', 'admin', '123')
+
+
+
+	INSERT INTO NHANKHAU
+	VALUES (1, N'Nguyễn Kiên', '2141', '082814', 'HCM', 'hk1.jpg', null, 'kh1', 'kh1')
+
+	
+	INSERT INTO HOPDONG
+	VALUES (1, '5/9/2018', '11/9/2018', 1, 'A2', 6000000, 6, 6)
+	UPDATE CANHO
+	SET TinhTrang = N'Đã thuê'
+	where MaCanHo = 'A2'
+	INSERT INTO PHIEUTHUTIEN
+	VALUES (1, 1, 1, '5/9/2018', 6, 36000000)
+
+	--SELECT * FROM PHIEUTHUTIEN
+--INSERT INTO HOPDONG
+--VALUES (1, '5/9/2018', 1, 'A2', 6000000, 6)
+--SELECT * FROM HOPDONG
+
+--CREATE PROCEDURE sp_Themhopdong @Mahopdong integer, @Ngaybatdauthue DateTime, @Mank integer, @Macanho varchar(10), @Gia integer, @Thoigianthue integer
+--AS  
+--begin
+--		insert into HOPDONG Values(@Mahopdong, @Ngaybatdauthue, @Mank, @Macanho, @Gia, @Thoigianthue)
+--		update CANHO set TinhTrang = N'Đã thuê' where MaCanHo = @Macanho
+--end
